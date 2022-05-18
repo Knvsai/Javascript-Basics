33753d85b7ae4082ca071e2cb2feaa76b6758d98
Knvsai <knvsai0007@gmail.com>
2022-05-18 18:37:19 +0530

    Day-5

---
+++ 5.Remove-first-n-characters-of-string.js
@@ -0,0 +1,8 @@
+// Write a function that a string (a) as argument
+// Create an object that has a property with key 'key' and a value of a
+// Return the object
+
+function myFunction(a) {
+   
+    return a.slice(3,a.length)
+ }
