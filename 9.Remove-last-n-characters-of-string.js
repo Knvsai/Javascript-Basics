9d9073357c645fe58c28873d4d5eb5f22efefa57
Knvsai <knvsai0007@gmail.com>
2022-05-20 10:35:06 +0530

    day-7

---
+++ 9.Remove-last-n-characters-of-string.js
@@ -0,0 +1,12 @@
+// Write a function that takes a string (a) as argument
+// Remove the last 3 characters of a
+// Return the result
+
+function myFunction(a) {
+   
+    return a.slice(0,a.length-3);
+ }
+ //or
+ function myFunction(a) {
+    return a.slice(0, -3);
+ }
